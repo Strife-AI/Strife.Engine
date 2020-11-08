3fb2f11ea2d2383b85d338db4dfa06b316e8a363
Michael Wilder <mwilder.w@gmail.com>
2020-11-08 13:24:01 -0500

    Move data to NetComponent

---
+++ src/Components/NetComponent.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <vector>
+
+#include "Math/Vector2.hpp"
+#include "Memory/CircularQueue.hpp"
+#include "Scene/EntityComponent.hpp"
+
+enum class PlayerCommandStatus
+{
+    NotStarted,
+    InProgress,
+    Complete
+};
+
+struct PlayerCommand
+{
+    unsigned char fixedUpdateCount;
+    unsigned char keys;
+    unsigned int id;
+
+    // Client only
+    float timeRecorded;
+
+    // Server only
+    Vector2 positionAtStartOfCommand;
+    PlayerCommandStatus status = PlayerCommandStatus::NotStarted;
+    int fixedUpdateStartId;
+};
+
+struct PlayerSnapshot
+{
+    Vector2 position;
+    float time;
+    int commandId;
+};
+
+DEFINE_COMPONENT(NetComponent)
+{
+    PlayerCommand* GetCommandById(int id);
+    Vector2 PositionAtFixedUpdateId(int fixedUpdateId, int currentFixedUpdateId);
+    Vector2 GetSnapshotPosition(float time);
+    void AddSnapshot(const PlayerSnapshot & snapshot);
+
+    int netId;
+    unsigned int keyBits = 0;
+    Vector2 smoothVelocity;
+    unsigned int nextCommandSequenceNumber = 0;
+    unsigned int lastServerSequenceNumber = 0;
+    unsigned int lastServedExecuted = 0;
+    Vector2 positionAtStartOfCommand;
+
+    unsigned int clientClock = 0;
+
+    CircularQueue<PlayerCommand, 128> commands;
+
+    bool isClientPlayer = false;
+    std::vector<PlayerSnapshot> snapshots;
+};
---
+++ src/Net/ReplicationManager.cpp
@@ -0,0 +1,13 @@
+#include "ReplicationManager.hpp"
+
+#include "Scene/Scene.hpp"
+
+void ReplicationManager::ComputeWorldDiff(WorldState& before, WorldState& after, WorldDiff& outDiff)
+{
+
+}
+
+WorldState ReplicationManager::GetWorldState(Scene* scene)
+{
+
+}
