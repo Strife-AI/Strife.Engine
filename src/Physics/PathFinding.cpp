4465a88acc747c82235d43e001aacdb90e3c1436
Michael Wilder <mwilder.w@gmail.com>
2020-11-16 22:37:53 -0500

    3 player demo

--- src/Physics/PathFinding.cpp
+++ src/Physics/PathFinding.cpp
@@ -111,7 +111,7 @@ void PathFinderService::ReceiveEvent(const IEntityEvent& ev)
     }
     else if(auto renderEvent = ev.Is<RenderEvent>())
     {
-        Visualize(renderEvent->renderer);
+        //Visualize(renderEvent->renderer);
     }
 }
 
