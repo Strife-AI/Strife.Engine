184c6a6d363fb79f177ab8a33e552875b66354da
Michael Wilder <mwilder.w@gmail.com>
2020-11-27 19:43:11 -0500

    Get it building again

--- Strife.ML/NewStuff.cpp
+++ Strife.ML/NewStuff.cpp
@@ -1,2 +1,22 @@
 #include "NewStuff.hpp"
 #include "Scene/BaseEntity.hpp"
+
+//struct MakeDecisionWorkItem : ThreadPoolWorkItem<SerializedModel>
+//{
+//
+//
+//    std::shared_ptr<INeuralNetwork> network;
+//};
+//
+//struct NewDecider
+//{
+//    std::shared_ptr<MakeDecisionWorkItem> MakeDecision(const std::shared_ptr<InputType[]>& input, int inputSize)
+//    {
+//        auto threadPool = ThreadPool::GetInstance();
+//        auto workItem = std::make_shared<MakeDecisionWorkItem>(network, input, inputSize);
+//        threadPool->StartItem(workItem);
+//        return workItem;
+//    }
+//
+//    std::shared_ptr<INeuralNetwork> network;
+//};
