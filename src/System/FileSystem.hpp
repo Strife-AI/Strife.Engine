7ebcf53e82f1fbfe1e15aeaaab664e722e50029f
Michael Wilder <mwilder.w@gmail.com>
2020-12-23 21:52:32 -0500

    Implement file uploading

--- src/System/FileSystem.hpp
+++ src/System/FileSystem.hpp
@@ -1,6 +1,9 @@
 #pragma once
+
 #include <cstdio>
 #include <string>
+#include <vector>
 
 FILE* OpenFile(const char* path, const char* mode);
 bool TryReadFileContents(const char* path, std::string& result);
+bool TryReadFileContents(const char* path, std::vector<unsigned char>& result);
