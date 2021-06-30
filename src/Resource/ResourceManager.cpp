a227fd9a4d43f25a4d3ccb7fda4a0841d32c8bfa
Brendan Mulcahy <mulcahy.software@gmail.com>
2021-06-30 09:52:19 -0400

    Fix dangling pointer issue

--- src/Resource/ResourceManager.cpp
+++ src/Resource/ResourceManager.cpp
@@ -103,7 +103,9 @@ void ResourceManager::LoadContentFile(const char* filePath)
 
         ResourceSettings settings;
         settings.resourceName = name.c_str();
-        settings.path = path.string().c_str();
+
+        auto resourcePath = path.string();
+    	settings.path = resourcePath.c_str();
         settings.resourceType = type.c_str();
         settings.attributes = attributes;
 
